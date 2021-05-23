343dad313d733faee7c99765c9e886f71d8a38ae
DanielTuttos <tutorialesdanielromero@gmail.com>
2021-05-23 17:04:32 -0500

    version 1 de calendar app

---
+++ public/precache-manifest.ecd706cedb27e064fd6ad4dc4447dd92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc652761c3a47eb188fe6b1c827b2521",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc2608194ff2118b43dc",
+    "url": "/static/css/2.ceee16bc.chunk.css"
+  },
+  {
+    "revision": "b47212399368934845b0",
+    "url": "/static/css/main.ac97ee01.chunk.css"
+  },
+  {
+    "revision": "fc2608194ff2118b43dc",
+    "url": "/static/js/2.3164cea9.chunk.js"
+  },
+  {
+    "revision": "62dc9784057de1bc85a8684fec455f1b",
+    "url": "/static/js/2.3164cea9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b47212399368934845b0",
+    "url": "/static/js/main.10f86c86.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
